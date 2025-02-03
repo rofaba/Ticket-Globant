c1a12693b36cb2e3c886b1dbe178af9689b54b01
Rodrigo Faure <rodrigo.faure@globant.com>
2025-02-03 14:11:45 +0100

    feat: MainEvent Styles

--- src/components/MainEvent.jsx
+++ src/components/MainEvent.jsx
@@ -7,6 +7,9 @@ function MainEvent() {
     <img src="https://picsum.photos/550/350" alt="main_event" />
     <h3>Artista principal</h3>
     <p>El mejor evento de la historia</p>
+    <div className="concert_date">
+        <p>Fecha</p> <p>Hora</p>
+    </div>
     </div>
     )
     
