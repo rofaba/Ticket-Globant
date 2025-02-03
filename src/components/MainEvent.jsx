3b3ebf532ee4f630dfc0484956ea30aab64d9b1f
Rodrigo Faure <rodrigo.faure@globant.com>
2025-02-03 14:02:46 +0100

    feat(main-event): add MainEvent component

---
+++ src/components/MainEvent.jsx
@@ -0,0 +1,14 @@
+import './MainEvent.css'
+
+
+function MainEvent() {
+    return(
+    <div className="event_container">
+    <img src="https://picsum.photos/550/350" alt="main_event" />
+    <h3>Artista principal</h3>
+    <p>El mejor evento de la historia</p>
+    </div>
+    )
+    
+    }
+    export default MainEvent;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import ConcertList from "../components/ConcertList";
+import MainEvent from "../components/MainEvent"
 
 function Home() {
   return (
     <div>
+      <MainEvent />
       <h1>Conciertos Disponibles</h1>
       <ConcertList />
     </div>
