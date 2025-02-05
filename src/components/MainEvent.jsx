1ed2a922cc0b793a293eb45b61affa09ac5d0bd4
Rodrigo Faure <rodrigo.faure@globant.com>
2025-02-05 12:38:40 +0100

    feat(MainEvent):  the main event image changes at regular time intervals

--- src/components/MainEvent.jsx
+++ src/components/MainEvent.jsx
@@ -1,17 +1,33 @@
 import './MainEvent.css'
 import concerts from '../data/concerts';
+import { useState, useEffect } from 'react';
 
 function MainEvent() {
-    let randomindex = Math.floor(Math.random() * concerts.length)
+
+    const [concert, setConcert] = useState(concerts[0]);
+    
+    useEffect(() => {  
+        function getRandomConcert() {
+        let index = Math.floor(Math.random() * concerts.length);
+        setConcert(concerts[index])
+        }
+
+        const intervalId = setInterval(getRandomConcert, 10000);
+        return () => clearInterval(intervalId);
+
+    }, [concert]);
+
+// por ahora todos los conciertos tienen la misma src al mismo tiempo, pareciera no funciona pero funciona :D)
 
     return(
     <div className="event_container">
-        <img src={concerts[randomindex].src} />
+        <img src={concert.src} />
             <div className="concert-data">
-                <h2>{concerts[randomindex].name}</h2>
-                <p>{concerts[randomindex].description}</p>
-                <p>{concerts[randomindex].location}</p> 
-                <p>{concerts[randomindex].date}</p>
+                <h2>{concert.name}</h2>
+                <p>{concert.description}</p>
+                <p>{concert.location}</p> 
+                <p>{concert.date}</p>
+                <p>IR DETALLE</p>
             </div>
     </div>
     )
